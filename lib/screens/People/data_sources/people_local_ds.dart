3aaa38986a9d99c35d89fb267b0652f2b120e615
Aminsaveh <aminsaveh@hotmail.com>
2023-05-18 00:51:41 +0430

    Add People Data Source

--- lib/screens/People/data_sources/people_local_ds.dart
+++ lib/screens/People/data_sources/people_local_ds.dart
@@ -1,3 +1,4 @@
+import 'package:negarestan/screens/People/usecases/follow_usecase.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../interfaces/people_data_source_interface.dart';
@@ -8,5 +9,10 @@ class PeopleLocalDataSource implements PeopleDataSourceInterface {
   PeopleLocalDataSource({
     required this.sharedPreferences,
   });
+
+  @override
+  Future<String> follow(FollowRequest request) {
+    throw UnimplementedError();
+  }
 
 }
--- lib/screens/People/interfaces/people_repository_interface.dart
+++ lib/screens/People/interfaces/people_repository_interface.dart
@@ -1,4 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:negarestan/screens/People/usecases/follow_usecase.dart';
 import '../../../../core/error/failures.dart';
+
 abstract class PeopleRepositoryInterface {
+  Future<Either<Failure, String>> follow(FollowRequest request);
 }
