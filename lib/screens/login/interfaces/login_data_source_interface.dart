de4774ecdef44c7fbbe168cbb9821988e585f3b3
Aminsaveh <aminsaveh@hotmail.com>
2023-05-18 00:24:18 +0430

    Add Login Data Source

--- lib/screens/login/interfaces/login_data_source_interface.dart
+++ lib/screens/login/interfaces/login_data_source_interface.dart
@@ -1,5 +1,7 @@
 import '../usecases/login_usecase.dart';
+import '../usecases/sign_up_usecase.dart';
 
 abstract class LoginDataSourceInterface {
   Future<String> login(LoginRequest request);
+  Future<String> signUp(SignUpRequest request);
 }
--- lib/screens/login/interfaces/login_repository_interface.dart
+++ lib/screens/login/interfaces/login_repository_interface.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
 import '../usecases/login_usecase.dart';
+import '../usecases/sign_up_usecase.dart';
 
 abstract class LoginRepositoryInterface {
   Future<Either<Failure, String>> login(LoginRequest request);
+  Future<Either<Failure, String>> signUp(SignUpRequest request);
 }
