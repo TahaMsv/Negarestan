bd517ddd5ab79889314fdfaf7bc1922626136c59
Aminsaveh <aminsaveh@hotmail.com>
2023-05-19 20:01:37 +0430

    Modify People

--- lib/screens/People/data_sources/people_remote_ds.dart
+++ lib/screens/People/data_sources/people_remote_ds.dart
@@ -1,28 +1,7 @@
-import 'package:negarestan/screens/People/usecases/follow_usecase.dart';
 import 'package:network_manager/network_manager.dart';
 
 import '../../../core/constants/apis.dart';
 import '../../../core/error/exception.dart';
 import '../interfaces/people_data_source_interface.dart';
 
-class PeopleRemoteDataSource implements PeopleDataSourceInterface {
-  @override
-  Future<bool> follow(FollowRequest request) async {
-    NetworkRequest followNR = NetworkRequest(api: Apis.baseUrl + Apis.follow, data: request.toJson(), timeOut: const Duration(seconds: 15));
-    NetworkResponse followResponse = await followNR.post();
-    if (followResponse.responseStatus) {
-      try {
-        bool success = followResponse.responseBody["success"];
-        return success;
-      } catch (e, trace) {
-        throw ParseException(message: e.toString(), trace: trace);
-      }
-    } else {
-      throw ServerException(
-        code: followResponse.responseCode,
-        message: followResponse.extractedMessage!,
-        trace: StackTrace.fromString("PeopleRemoteDataSource.login"),
-      );
-    }
-  }
-}
+class PeopleRemoteDataSource implements PeopleDataSourceInterface {}
--- lib/screens/People/people_repository.dart
+++ lib/screens/People/people_repository.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'package:dartz/dartz.dart';
-import 'package:negarestan/screens/People/usecases/follow_usecase.dart';
 import '../../core/error/exception.dart';
 import '../../core/error/failures.dart';
 import '../../core/platform/network_info.dart';
@@ -11,27 +10,5 @@ class PeopleRepository implements PeopleRepositoryInterface {
   final PeopleRemoteDataSource peopleRemoteDataSource;
   final NetworkInfo? networkInfo;
   PeopleRepository({required this.peopleRemoteDataSource, required this.networkInfo, });
-
-  @override
-  Future<Either<Failure, bool>> follow(FollowRequest followRequest) async {
-    if (await networkInfo!.isConnected) {
-      try {
-        bool success = await peopleRemoteDataSource.follow(followRequest);
-        return Right(success);
-      } on AppException catch (e) {
-        return Left(ServerFailure.fromAppException(e));
-      }
-    } else {
-      return Left(
-        ConnectionFailure.fromAppException(
-          ConnectionException(
-            message: "You are not connected to internet!",
-            trace: StackTrace.fromString("People repository: login"),
-          ),
-        ),
-      );
-      // }
-    }
-  }
 
 }
